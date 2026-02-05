6e607bfdff585db840ee871cc320b88d816555ce
wellington <wellington.h.s.oliveira@hotmail.com>
2026-02-05 07:24:49 -0300

    repositorios criados

---
+++ Teste Prático - Autoflex/src/main/java/com/autoflex/repository/RawMaterialRepository.java
@@ -0,0 +1,7 @@
+package com.autoflex.repository;
+
+import com.autoflex.entity.RawMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawMaterialRepository extends JpaRepository<RawMaterial, Long> {
+}
